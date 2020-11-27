fb79d5761a3c2d82fab67743639f5a1cc4af3eb2
Nikita Tutik <40828315+NikitaTutik@users.noreply.github.com>
2020-11-27 08:51:35 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,12 +1,4 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from .serializers import PostSerializer
-from .models import Post
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
-from rest_framework import mixins
 
 
 class PostListCreateView(generics.ListCreateAPIView):
