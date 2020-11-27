a62308c6b9a3dbda510ff0d99cd2186b7d32fea9
Nikita Tutik <40828315+NikitaTutik@users.noreply.github.com>
2020-11-27 08:55:30 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
-
+from .serializers import PostSerializer
+from .models import Post
 
 class PostListCreateView(generics.ListCreateAPIView):
     serializer_class = PostSerializer
